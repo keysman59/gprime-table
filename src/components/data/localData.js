5d96db8c7d9f17ce32ee3f1dbc65ea69e7f92891
unknown <dima>
2023-01-05 20:15:17 +0300

    some

--- src/components/data/localData.js
+++ src/components/data/localData.js
@@ -170,7 +170,271 @@ let localResult = [
     originalAttributes: 'Array[45]',
     searchType: '',
     shortAttributes: 'Array[16]',    
-}]
+},
+{
+    attributes: [{
+        "name": "Статус",
+        "value": "Действующий",
+        "originalName": "status"
+    }, {
+        "name": "Вид документа",
+        "value": "Программа комплексного развития систем социальной инфраструктуры",
+        "originalName": "document_type"
+    }, {
+        "name": "Наименование документа",
+        "value": "Программа комплексного развития систем социальной инфраструктуры МО «Важинское городское поселение Подпорожского муниципального района Ленинградской области» на период до 2030 года",
+        "originalName": "doc_name"
+    }, {
+        "name": "Служба ИСОГД",
+        "value": "Адм Подпорожский МР",
+        "originalName": "isogd_service"
+    }, {
+        "name": "Территория",
+        "value": "Важинское городское поселение",
+        "originalName": "territory"
+    }, {
+        "name": "Кадастровый номер",
+        "value": "47:05",
+        "originalName": "cadastral_number"
+    }, {
+        "name": "Описание территории",
+        "value": "Важинское городское поселение",
+        "originalName": "territory_description"
+    }, {
+        "name": "Регистрационный номер в ГИСОГД",
+        "value": "41636000_14_000189",
+        "originalName": "registration_number"
+    }, {
+        "name": "Дата регистрации",
+        "value": "01.02.2021",
+        "originalName": "registration_date"
+    }, {
+        "name": "Уполномоченный орган, разместивший данные",
+        "value": "Адм Подпорожский МР",
+        "originalName": "data_placement_organ"
+    }, {
+        "name": "Заказчик",
+        "value": "Администрация МО \"Важинское городское поселение\"",
+        "originalName": "customer_name"
+    }, {
+        "name": "Кем утвержден",
+        "value": "Администрация МО \"Важинское городское поселение\"",
+        "originalName": "approved_by"
+    }, {
+        "name": "Вид утверждающего документа",
+        "value": "Постановление",
+        "originalName": "approval_document"
+    }, {
+        "name": "№ утверждающего документа",
+        "value": "404",
+        "originalName": "approval_document_number"
+    }, {
+        "name": "Дата утверждающего документа",
+        "value": "23.11.2017",
+        "originalName": "approval_document_date"
+    }, {
+        "name": "Разработчик",
+        "value": "Администрация МО \"Важинское городское поселение\"",
+        "originalName": "developer"
+    }, {
+        "name": "Год разработки",
+        "value": 2017,
+        "originalName": "development_year"
+    }],
+    crs: '',
+    entityField: "id",
+    feature: [],
+    featureId: 'l_isogd_documents_without_inzh',
+    filePlace: "8",
+    geometry: [],
+    hideEmpty: true,
+    isShare: true,
+    layerField: "a161551f-d4e5-4906-a708-402246944a34",
+    layerId: "a161551f-d4e5-4906-a708-402246944a34",
+    layerName: "fpd:l_isogd_documents_without_inzh",
+    name: "ГИСОГД. Программа комплексного развития систем транспортной инфраструктуры МО «Важинское городское поселение Подпорожского муниципального района Ленинградской области» на период до 2030 года, 2017-11-24",
+    objectIdValue: "e34b1015-3856-4280-b215-f5ee117b0c6f",
+    originalAttributes: 'Array[45]',
+    searchType: '',
+    shortAttributes: 'Array[16]',    
+},
+{
+    attributes: [{
+        "name": "Статус",
+        "value": "Действующий",
+        "originalName": "status"
+    }, {
+        "name": "Вид документа",
+        "value": "Программа комплексного развития систем социальной инфраструктуры",
+        "originalName": "document_type"
+    }, {
+        "name": "Наименование документа",
+        "value": "Программа комплексного развития систем социальной инфраструктуры МО «Важинское городское поселение Подпорожского муниципального района Ленинградской области» на период до 2030 года",
+        "originalName": "doc_name"
+    }, {
+        "name": "Служба ИСОГД",
+        "value": "Адм Подпорожский МР",
+        "originalName": "isogd_service"
+    }, {
+        "name": "Территория",
+        "value": "Важинское городское поселение",
+        "originalName": "territory"
+    }, {
+        "name": "Кадастровый номер",
+        "value": "47:05",
+        "originalName": "cadastral_number"
+    }, {
+        "name": "Описание территории",
+        "value": "Важинское городское поселение",
+        "originalName": "territory_description"
+    }, {
+        "name": "Регистрационный номер в ГИСОГД",
+        "value": "41636000_14_000189",
+        "originalName": "registration_number"
+    }, {
+        "name": "Дата регистрации",
+        "value": "01.02.2021",
+        "originalName": "registration_date"
+    }, {
+        "name": "Уполномоченный орган, разместивший данные",
+        "value": "Адм Подпорожский МР",
+        "originalName": "data_placement_organ"
+    }, {
+        "name": "Заказчик",
+        "value": "Администрация МО \"Важинское городское поселение\"",
+        "originalName": "customer_name"
+    }, {
+        "name": "Кем утвержден",
+        "value": "Администрация МО \"Важинское городское поселение\"",
+        "originalName": "approved_by"
+    }, {
+        "name": "Вид утверждающего документа",
+        "value": "Постановление",
+        "originalName": "approval_document"
+    }, {
+        "name": "№ утверждающего документа",
+        "value": "404",
+        "originalName": "approval_document_number"
+    }, {
+        "name": "Дата утверждающего документа",
+        "value": "23.11.2017",
+        "originalName": "approval_document_date"
+    }, {
+        "name": "Разработчик",
+        "value": "Администрация МО \"Важинское городское поселение\"",
+        "originalName": "developer"
+    }, {
+        "name": "Год разработки",
+        "value": 2017,
+        "originalName": "development_year"
+    }],
+    crs: '',
+    entityField: "id",
+    feature: [],
+    featureId: 'l_isogd_documents_without_inzh',
+    filePlace: "8",
+    geometry: [],
+    hideEmpty: true,
+    isShare: true,
+    layerField: "a161551f-d4e5-4906-a708-402246944a34",
+    layerId: "a161551f-d4e5-4906-a708-402246944a34",
+    layerName: "fpd:l_isogd_documents_without_inzh",
+    name: "ГИСОГД. Программа комплексного развития систем транспортной инфраструктуры МО «Важинское городское поселение Подпорожского муниципального района Ленинградской области» на период до 2030 года, 2017-11-24",
+    objectIdValue: "e34b1015-3856-4280-b215-f5ee117b0c6f",
+    originalAttributes: 'Array[45]',
+    searchType: '',
+    shortAttributes: 'Array[16]',    
+},
+{
+    attributes: [{
+        "name": "Статус",
+        "value": "Действующий",
+        "originalName": "status"
+    }, {
+        "name": "Вид документа",
+        "value": "Программа комплексного развития систем социальной инфраструктуры",
+        "originalName": "document_type"
+    }, {
+        "name": "Наименование документа",
+        "value": "Программа комплексного развития систем социальной инфраструктуры МО «Важинское городское поселение Подпорожского муниципального района Ленинградской области» на период до 2030 года",
+        "originalName": "doc_name"
+    }, {
+        "name": "Служба ИСОГД",
+        "value": "Адм Подпорожский МР",
+        "originalName": "isogd_service"
+    }, {
+        "name": "Территория",
+        "value": "Важинское городское поселение",
+        "originalName": "territory"
+    }, {
+        "name": "Кадастровый номер",
+        "value": "47:05",
+        "originalName": "cadastral_number"
+    }, {
+        "name": "Описание территории",
+        "value": "Важинское городское поселение",
+        "originalName": "territory_description"
+    }, {
+        "name": "Регистрационный номер в ГИСОГД",
+        "value": "41636000_14_000189",
+        "originalName": "registration_number"
+    }, {
+        "name": "Дата регистрации",
+        "value": "01.02.2021",
+        "originalName": "registration_date"
+    }, {
+        "name": "Уполномоченный орган, разместивший данные",
+        "value": "Адм Подпорожский МР",
+        "originalName": "data_placement_organ"
+    }, {
+        "name": "Заказчик",
+        "value": "Администрация МО \"Важинское городское поселение\"",
+        "originalName": "customer_name"
+    }, {
+        "name": "Кем утвержден",
+        "value": "Администрация МО \"Важинское городское поселение\"",
+        "originalName": "approved_by"
+    }, {
+        "name": "Вид утверждающего документа",
+        "value": "Постановление",
+        "originalName": "approval_document"
+    }, {
+        "name": "№ утверждающего документа",
+        "value": "404",
+        "originalName": "approval_document_number"
+    }, {
+        "name": "Дата утверждающего документа",
+        "value": "23.11.2017",
+        "originalName": "approval_document_date"
+    }, {
+        "name": "Разработчик",
+        "value": "Администрация МО \"Важинское городское поселение\"",
+        "originalName": "developer"
+    }, {
+        "name": "Год разработки",
+        "value": 2017,
+        "originalName": "development_year"
+    }],
+    crs: '',
+    entityField: "id",
+    feature: [],
+    featureId: 'l_isogd_documents_without_inzh',
+    filePlace: "8",
+    geometry: [],
+    hideEmpty: true,
+    isShare: true,
+    layerField: "a161551f-d4e5-4906-a708-402246944a34",
+    layerId: "a161551f-d4e5-4906-a708-402246944a34",
+    layerName: "fpd:l_isogd_documents_without_inzh",
+    name: "ГИСОГД. Программа комплексного развития систем транспортной инфраструктуры МО «Важинское городское поселение Подпорожского муниципального района Ленинградской области» на период до 2030 года, 2017-11-24",
+    objectIdValue: "e34b1015-3856-4280-b215-f5ee117b0c6f",
+    originalAttributes: 'Array[45]',
+    searchType: '',
+    shortAttributes: 'Array[16]',    
+}
+
+
+]
 
 
 export {
